505d6125d61911ce0ae250e9ade1cb7ff69a58b5
gracekyalo <gracendanu27@gmail.com>
2022-10-01 15:13:24 +0300

    Complete

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,32 @@
-import React from "react";
-import { movies } from "../data";
+import React, {useEffect, useState} from "react";
+// import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+const[movies,setMovies] =useState([]);
+// const params= useParams()
+
+useEffect(() =>{
+  fetch("/movies")
+  .then(r => r.json())
+  .then(movies => setMovies(movies))
+}, [])
+
+const{title, name, genres} = movies
+
+
+  return (
+    <>
+    <h1> Movies Page</h1>
+    <div>
+<h2>{title}</h2>
+<h3>{name}</h3>
+<div > {genres}</div>
+
+    </div>
+
+    </>
+  )
+   
 }
 
 export default Movies;
